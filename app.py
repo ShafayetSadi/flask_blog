6448a87832fb89965929ad35e33cf20ef3b6eccd
Shafayet Sadi <43206214+ShafayetSadi@users.noreply.github.com>
2019-04-16 12:15:36 +0600

    My flask blog

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Shafayet Sadi",
+        "title": "Hello Word",
+        'content': 'My first post.I have written a python program',
+        'date': 'April 16, 2019',
+    },
+    {
+        "author": "Sifat Jaman",
+        "title": "Hello Hexa Core",
+        'content': 'My first post.Welcome to Hexa Core Assylum',
+        'date': 'April 16, 2019',
+    },
+]
 
 @app.route('/')
 @app.route('/home')
 
 def home():
-    return "<h1>Home Page<h1\>"
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About Page<h1\>'
+    return render_template('about.html', title='About Page')
 
 if __name__ == '__main__':
     app.run(debug=True)
