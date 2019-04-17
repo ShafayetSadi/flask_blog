ce01b9394d2ad15ae403ffde3e643fa23c5cff62
Shafayet Sadi <43206214+ShafayetSadi@users.noreply.github.com>
2019-04-17 18:31:08 +0600

    Hey

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import ResistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0270e77bd9e8c8ab55ab4b2cbcccdaeb'
 
 posts = [
     {
@@ -25,6 +28,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About Page')
+
+@app.route('/register')
+def register():
+    form = ResistrationForm()
+    return render_template('register.html', title="Register", form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Login", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
