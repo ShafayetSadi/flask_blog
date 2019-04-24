d04b896635f5fd19f0eb8b435f585894ccc8b807
shafayetsadi <shafayet.sadi@gmail.com>
2019-04-24 19:58:03 +0600

    Updated picture size

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -5,6 +5,7 @@ from flask_app import app, db, bcrypt
 from flask_login import login_user, logout_user, current_user, login_required
 import secrets
 import os
+from PIL import Image
 
 posts = [
     {
@@ -72,7 +73,10 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
     return picture_fn
 
 
