eb9aab7c4e9052a4642f66642b50183e5d5e36c5
shafayetsadi <shafayet.sadi@gmail.com>
2019-05-10 12:02:01 +0600

    Added new features

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -11,7 +11,8 @@ from PIL import Image
 @app.route('/')
 @app.route('/home')
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
